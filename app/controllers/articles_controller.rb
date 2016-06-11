454fb93361d029412e2246b0d940740be7627159
Kevin <kevirl@gmail.com>
2016-06-11 15:14:04 +0000

    further updates on Dev

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     def new
       @article = Article.new
     end
+    
+    def edit
+      @article = Article.find(params[:id])
+    end
 
     def create
       @article = Article.new(article_params)
@@ -21,7 +25,17 @@ class ArticlesController < ApplicationController
         render 'new'
       end
     end
+
+    def update
+      @article = Article.find(params[:id])
  
+      if @article.update(article_params)
+        redirect_to @article
+      else
+       render 'edit'
+      end
+    end
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
